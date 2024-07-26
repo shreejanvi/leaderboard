b2946dcae4b722005670ce2468ea932408fd231c
Shree Janvi <151512837+shreejanvi@users.noreply.github.com>
2024-07-26 10:55:48 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,7 @@ const scoresSlice = createSlice({
     addScore: (state, action) => {
       state.push(action.payload);
       state.sort((a, b) => (a.time > b.time ? 1 : -1));
-      if (state.length > 10) state.pop();
+      if (state.length > 10) state.shift();
     },
   },
 });
